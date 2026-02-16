f06ddd6d6915a61a42b811c54df6ce7ca9dbf4f2
ryanabin16-jpg <ryanabin16@gmail.com>
2026-02-16 17:18:46 +1100

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,13 @@
+a=10
+b=6.7
+c="hi i am python"
+d=True
+print("type of a=",type(a))
+print("type of b=",type(b))
+print("type of c=",type(c))
+print("type of d=",type(d))
+print("after typecasting")
+b=int(b)
+a=float(a)
+print("type of a=",type(a))
+print("type of b=",type(b))
