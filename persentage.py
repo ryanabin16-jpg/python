d883a1295d7ae4324f7cf2a37a17c797c03417f2
ryanabin16-jpg <ryanabin16@gmail.com>
2026-02-24 19:04:23 +1100

    Add files via upload

---
+++ persentage.py
@@ -0,0 +1,10 @@
+print("enter marks of 5 subjects out of 100")
+math=int(input("math="))
+sci=int(input("science="))
+c=int(input("coding="))
+eng=int(input("english="))
+sport=int(input("sport"))
+total= math+sci+c+eng+sport
+perc=(total/500)*100 
+print("total of your marks=",total)
+print("percantage=",perc)
